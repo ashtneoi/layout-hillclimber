7b43fbe3484bf8699fd1d6cb13082197110694c8
ashtneoi <ashtneoi@gmail.com>
2021-08-25 21:39:33 -0700

    Idk what this is

--- test-layout.sh
+++ test-layout.sh
@@ -1,3 +1,7 @@
+echo
+echo -e "1234 7890\nqdrw fup;\nasht neoi\nzxmc l,.'" \
+    | tr "12347890qdrwfup;ashtneoizxmcl,.'" "$@" | tr a-z A-Z
+echo
 trap "stty sane" EXIT
 stty min 1 time 0 -icanon -echo
-stdbuf -o0 tr "$@"
+stdbuf -o0 tr "12347890qdrwfup;ashtneoizxmcl,.'" "$@"
